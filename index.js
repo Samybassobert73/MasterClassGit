be06440c54c7de307775b6df97da2f6875cd2832
samy <samy.basso-bert@epitech.eu>
2024-06-09 14:36:35 +0200

    design

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 console.log('refacto caca chef !');
 console.log('refacto2');
+console.log('Hello World!!!!');
+console.log('hello Samy');
+console.log('hello Samy2');
+console.log('hello Samy3');
+console.log('hello Samy4');
